61e3781b02881b2055d1e67312495fc1d5078b97
Thomas Claessens <tom@itcl.io>
2023-02-19 15:25:38 +0100

    ...

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,7 @@ APP_ROOT=/data/www/businesspadeltour.be
 
 mv ${APP_ROOT}/docker-compose.yml ${APP_ROOT}/docker-compose.back.yml 
 mv ${APP_ROOT}/docker-compose.prod.yml ${APP_ROOT}/docker-compose.yml 
-
+mv ${APP_ROOT}/.env ${APP_ROOT}/.env.back
+cp ${APP_ROOT}/.env.prod ${APP_ROOT}/.env
 docker compose up -d
 
