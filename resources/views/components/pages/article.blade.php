3d586bd65459d4d0ed54a1f9bde09ea080af4668
Thomas Claessens <tom@itcl.io>
2023-02-19 14:29:15 +0100

    ...

---
+++ resources/views/components/pages/article.blade.php
@@ -0,0 +1,77 @@
+<x-layouts.home>
+    <!-- This example requires Tailwind CSS v3.0+ -->
+<!-- This example requires Tailwind CSS v3.0+ -->
+<div class="isolate bg-white">
+    <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+      <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
+        <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
+        <defs>
+          <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#9089FC"></stop>
+            <stop offset="1" stop-color="#FF80B5"></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+    <x-layouts.menu >
+    </x-layouts.menu>
+    <main>
+      <!--
+        This example requires some changes to your config:
+        
+        ```
+        // tailwind.config.js
+        module.exports = {
+          // ...
+          plugins: [
+            // ...
+            require('@tailwindcss/typography'),
+          ],
+        }
+        ```
+      -->
+      <div class="relative overflow-hidden bg-white py-16">
+        <div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+          <div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
+            <svg class="absolute top-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+              <defs>
+                <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+            </svg>
+            <svg class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+              <defs>
+                <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+            </svg>
+            <svg class="absolute bottom-12 left-full translate-x-32 transform" width="404" height="384" fill="none" viewBox="0 0 404 384">
+              <defs>
+                <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+            </svg>
+          </div>
+        </div>
+        <div class="relative px-6 lg:px-8">
+          <div class="mx-auto max-w-prose text-lg">
+            <h1>
+              <span class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">{{$article['intro_title']}}</span>
+            </h1>
+            <p class="mt-8 text-xl leading-8 text-gray-500">{{$article['intro_subtitle']}}</p>
+          </div>
+          <div class="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
+            {!!$article['content']!!}
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+  
+</x-layouts.home>
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+//use Laravel\Octane\Facades\Octane;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::post('livewire/message/{name}', '\Livewire\Controllers\HttpConnectionHandler');
+
+
+//Octane::route('GET','/{lang?}/index',function () {return new Response((new HomeController)->index());});
+Route::get('/register', function () {
+    return view('components.pages.register');
+});
+
+
+// Route::prefix('{lang}')->middleware('set.locale')->group(function () {
+//     Octane::route('GET','/index',function () {return new Response((new HomeController)->index());});
+// });
+
+Route::get('/', function () {
+    return new Response((new HomeController)->index());
+});
+Route::prefix('{locale}')->where(['locale' => '[a-zA-Z]{2}'])->middleware('set.locale')->group(function ($r) {
+
+    if (in_array(app('request')->segment(1), array_keys(config('app.locales')))) {
+        app()->setLocale(app('request')->segment(1));
+    }
+    Route::get('/index', function () {
+        return new Response((new HomeController)->index());
+    });
+    Route::get('/', function () {
+        return new Response((new HomeController)->index());
+    });
+    Route::get('/' . trans('route.articles'), function () {
+        return new Response((new ArticleController)->index());
+    });
+    Route::get('/' . trans('route.article').'/{slug}', function ($locale,$slug) {
+        return new Response((new ArticleController)->show($locale,$slug));
+    });
+    Route::get('/'. trans('route.register'), function () {
+        return view('components.pages.register');
+    });
+   // Route::get('/' . trans('route.article', []).'/{slug}', 'ArticleController@show')->name('article');
+    // Octane::route('GET','/en/index',function () {return new Response((new HomeController)->index());});
+});
