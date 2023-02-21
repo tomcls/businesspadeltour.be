4993759a0f8b19ae26b7558ec657f0766a18e568
Don Classius <tom@itcl.io>
2023-02-21 16:36:07 +0100

    ...

--- docker/prod/run.sh
+++ docker/prod/run.sh
@@ -9,15 +9,7 @@ docker rm -f ${name}
 docker build --file=${APP_ROOT}"/docker/prod/php8.2/Dockerfile"  -t padel/php .
 docker run --name ${name} \
   -v ${APP_ROOT}:/var/www/html \
-  -d padel/php 
+  -it padel/php 
 
-docker rm -f padel-nginx
-docker build --file=${APP_ROOT}"/docker/prod/nginx/Dockerfile"   -t padel/nginx .
 
-docker run   --name padel-nginx \
---link ${name} \
--p  71:80 \
--v  $APP_ROOT:/usr/share/nginx/html \
--v  $APP_ROOT/docker/prod/nginx/sites-available/default.conf:/etc/nginx/conf.d/default.conf \
--d padel/nginx
 
