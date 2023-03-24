0694b56059a2144725f850ef91c8a209755462ea
Thomas Claessens <tom@itcl.io>
2023-03-24 18:54:03 +0100

    ...

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -54,37 +54,37 @@ class SitemapXmlController extends Controller
             [
                 "loc"=>'https://businesspadeltour.be/en/news',
                 "lastmod"=>date('Y-m-d'),
-                "changefreq"=>'monthly',
+                "changefreq"=>'weekly',
                 "priority"=>'0.8',
             ],
             [
                 "loc"=>'https://businesspadeltour.be/fr/inscription',
                 "lastmod"=>date('Y-m-d'),
-                "changefreq"=>'monthly',
+                "changefreq"=>'weekly',
                 "priority"=>'0.8',
             ],
             [
                 "loc"=>'https://businesspadeltour.be/nl/register',
                 "lastmod"=>date('Y-m-d'),
-                "changefreq"=>'monthly',
+                "changefreq"=>'weekly',
                 "priority"=>'0.8',
             ],
             [
                 "loc"=>'https://businesspadeltour.be/en/register',
                 "lastmod"=>date('Y-m-d'),
-                "changefreq"=>'monthly',
+                "changefreq"=>'weekly',
                 "priority"=>'0.8',
             ],
             [
                 "loc"=>'https://businesspadeltour.be/fr/article/padel-3-faits-insolites-a-savoir-sur-le-padel',
                 "lastmod"=>date('Y-m-d'),
-                "changefreq"=>'monthly',
+                "changefreq"=>'weekly',
                 "priority"=>'0.8',
             ],
             [
                 "loc"=>'https://businesspadeltour.be/fr/article/le-business-padel-tour-un-tournoi-de-padel-interentreprises-en-belgique',
                 "lastmod"=>date('Y-m-d'),
-                "changefreq"=>'monthly',
+                "changefreq"=>'weekly',
                 "priority"=>'0.8',
             ],
         ];
