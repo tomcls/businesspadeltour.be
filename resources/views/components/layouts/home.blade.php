e7465913b0e8032967dfe26b9f0e7cb24b8fca3a
Thomas Claessens <tom@itcl.io>
2023-03-24 19:05:39 +0100

    ...

--- resources/views/components/layouts/home.blade.php
+++ resources/views/components/layouts/home.blade.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html  >
+<html lang="{{ str_replace('_', '-', App::currentLocale()) }}-" xmlns:og="http://ogp.me/ns#">
     <head>
         <title>{{ $title ?? env('APP_NAME') }}</title>
-        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html;" charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf_token" value="{{ csrf_token() }}"/>
         @vite(['resources/scss/app.scss'])
