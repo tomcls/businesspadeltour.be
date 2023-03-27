50132232ac72ad52a193cc80ec32565dda0d82f5
Thomas Claessens <tom@itcl.io>
2023-03-27 11:54:57 +0200

    clubs

--- resources/views/components/pages/articles.blade.php
+++ resources/views/components/pages/articles.blade.php
@@ -45,7 +45,52 @@
               </div>
             </div>
           </div>
-          
+          <div class="bg-white py-24 sm:py-32 " id="sponsor">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+              <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{__('home.sponsorTitle')}}</h2>
+              </div>
+              <div class="flex flex-1 items-center justify-center w-100 flex-row mt-10">
+                <ul role="list" class="items-center justify-center inline-flex space-x-10">
+                  <li>
+                    <a href="https://immo.vlan.be/fr"  target="_blank" > <img class="mx-auto w-50 items-center justify-center mt-7" src="{{url('/').'/images/sponsors/immovlan_mauve.png'}}" alt=""></a>
+                  </li>
+                  <li>
+                    <a href="https://www.newb19.be/home"  target="_blank" > <img class="mx-auto w-50 " src="{{url('/').'/images/sponsors/b19.png'}}" alt=""></a>
+                  </li>
+                  <!-- More people... -->
+                </ul>
+              </div>
+            </div>
+          </div>
+    
+          <div class="bg-white " id="sponsor">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+              <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{__('home.clubTitle')}}</h2>
+              </div>
+              <div class="flex flex-1 items-center justify-center w-100 flex-row mt-10">
+                <ul role="list" class="items-center justify-center inline-flex space-x-12">
+                  <li>
+                    <a href="https://tour-taxis.com/fr/event/world-padel-tour-2023-gare-maritime/" target="_blank"  ><img class="mx-auto w-50 items-center justify-center mt-7" src="{{url('/').'/images/clubs/padelclub.png'}}" alt=""></a>
+                  </li>
+                  <li>
+                    <a href="https://www.tereiken.be/"  target="_blank" ><img class="mx-auto w-50 items-center justify-center mt-7" src="{{url('/').'/images/clubs/tereiken.png'}}" alt=""></a>
+                  </li>
+                  <li>
+                    <a href="https://planetpadel.be/" target="_blank"  ><img class="mx-auto w-50 items-center justify-center mt-7" src="{{url('/').'/images/clubs/planetpadel.png'}}" alt=""></a>
+                  </li>
+                  <li>
+                    <a href="https://garrincha.be/" target="_blank" ><img class="mx-auto w-50 items-center justify-center mt-7" src="{{url('/').'/images/clubs/garrincha.jpg'}}" alt=""></a>
+                  </li>
+                  <li>
+                    <a href="https://elevenpadel.be/" target="_blank" ><img class="mx-auto w-50 items-center justify-center mt-7" src="{{url('/').'/images/clubs/eleven.jpg'}}" alt=""></a>
+                  </li>
+                  <!-- More people... -->
+                </ul>
+              </div>
+            </div>
+          </div>
        
     </main>
   </div>
