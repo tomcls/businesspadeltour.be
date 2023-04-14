43d0856d26a1763a004d90dae30f5474ab875d20
Thomas Claessens <tom@itcl.io>
2023-04-14 18:00:13 +0200

    analytics

--- resources/views/components/layouts/home.blade.php
+++ resources/views/components/layouts/home.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html  >
 <html lang="{{ str_replace('_', '-', App::currentLocale()) }}-" xmlns:og="http://ogp.me/ns#">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZJ8KWLG0VZ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-ZJ8KWLG0VZ');
+        </script>
         <title>{{ $title ?? env('APP_NAME') }}</title>
         <meta http-equiv="Content-Type" content="text/html;" charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
