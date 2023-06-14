258b6753872718fbab9b49d6d5410dc9d1bcf690
Thomas Claessens <tom@itcl.io>
2023-06-14 13:31:23 +0200

    ...

--- app/Http/Livewire/Sessions.php
+++ app/Http/Livewire/Sessions.php
@@ -8,7 +8,7 @@ class Sessions extends Component
 {
     public $showEditModal = false;
     public $tab = 'toursandtaxi'; // liege
-
+    public $queryString = ['tab'];
     public function render()
     {
         return view('livewire.sessions');
