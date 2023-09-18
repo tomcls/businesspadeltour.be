50e386a45d287c2ca51e7485a4f0db8fdf0bf646
Thomas Claessens <tom@itcl.io>
2023-09-18 12:10:17 +0200

    ...

--- resources/views/livewire/photos.blade.php
+++ resources/views/livewire/photos.blade.php
@@ -68,7 +68,7 @@
             <h2 class="text-xl  tracking-tight text-gray-400 sm:text-xl mb-10">{{__('sessions.tournoi')}} {{__('home.agendaWhen4')}}</h2>
             <div  class="mb-10   text-sm font-semibold"><a class="text-blue-500" href="{{env('APP_URL').'/images/nivelles.zip'}}">{{__('sessions.download')}}</a></div>
             <div class="-m-1 flex flex-wrap md:-m-2">
-                @for ($i = 259; $i < 390; $i++)
+                @for ($i = 259; $i < 341; $i++)
                     <div class="flex w-1/3 sm:w-1/4 flex-wrap" wire:ignore>
                         <div class="w-full p-1 md:p-2">
                             <a href="#" wire:click.prevent="$set('showEditModal', true)" @click="window.carousel.slideTo({{$i-1}});loadImages()" >
