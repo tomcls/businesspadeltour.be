39269738e0ec81dc18fb651e0d4114f3d9b0e950
Thomas Claessens <tom@itcl.io>
2023-09-18 12:07:49 +0200

    ...

--- resources/views/livewire/photos.blade.php
+++ resources/views/livewire/photos.blade.php
@@ -88,7 +88,7 @@
             <h2 class="text-xl  tracking-tight text-gray-400 sm:text-xl mb-10">{{__('sessions.tournoi')}} {{__('home.agendaWhen6')}}</h2>
             <div  class="mb-10 pt-10  text-sm font-semibold"><a class="text-blue-500" href="{{env('APP_URL').'/images/knokke.zip'}}">{{__('sessions.download')}}</a></div>
             <div class="-m-1 flex flex-wrap md:-m-2">
-                @for ($i = 341; $i < 502; $i++)
+                @for ($i = 341; $i < 501; $i++)
                     <div class="flex w-1/3 sm:w-1/4 flex-wrap" wire:ignore>
                         <div class="w-full p-1 md:p-2">
                             <a href="#" wire:click.prevent="$set('showEditModal', true)" @click="window.carousel.slideTo({{$i-1}});loadImages()" >
@@ -114,7 +114,7 @@
                     <!-- Carousel wrapper -->
                     
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-screen">
-                        @for ($i = 1; $i < 501; $i++)
+                        @for ($i = 1; $i < 500; $i++)
                          <!-- Item 1 -->
                             <div class="containerImage hidden duration-700 ease-in-out" id="carousel-item-{{$i}}"  >
                                 <img src="{{env('APP_URL').'/images/logo/logo-img.png'}}" data-src="{{env('APP_URL').'/images/sessions/session1_'.$i.'.jpg'}}"  class="absolute block sm:h-screen -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." wire:click.prevent="$set('showEditModal', false)">
