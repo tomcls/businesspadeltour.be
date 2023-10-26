086c6c77e440050d0391439b9f8e0d40da93a9a9
Thomas Claessens <tom@itcl.io>
2023-10-26 20:03:04 +0200

    ...

--- resources/views/components/layouts/menu.blade.php
+++ resources/views/components/layouts/menu.blade.php
@@ -293,7 +293,7 @@
                 <svg class="h-6 w-6 flex-shrink-0 color-light-blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
                 </svg>
-                <span class="ml-3 text-base font-medium text-gray-900">{{__('menu.program')}}</span>
+                <span class="ml-3 text-base font-medium text-gray-900">Planning 2024</span>
               </a>
   
               <a href="{{url('/').'/'.App::currentLocale().'#club'}}" x-on:click="isMobileNavOpen=false" class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50">
