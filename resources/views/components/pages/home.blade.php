16d952ee09fa3912da67c3955f3c21e12c2e8454
Thomas Claessens <tom@itcl.io>
2023-11-20 12:14:49 +0100

    ...

--- resources/views/components/pages/home.blade.php
+++ resources/views/components/pages/home.blade.php
@@ -173,7 +173,7 @@
                       <svg class="absolute top-1 left-1 h-6 w-6 color-light-blue" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                       </svg>
-                      <a href="https://www.tourtaxispadel.brussels/" class="color-light-blue"> {{__('home.clubContentPrefix2')}}:</a>
+                      <a href="https://www.tourtaxispadel.brussels/"  target="blank" class="color-light-blue"> {{__('home.clubContentPrefix2')}}:</a>
                     </dt>
                     <dd class="inline">{{__('home.clubContent2')}}</dd>
                   </div>
@@ -185,7 +185,7 @@
                       <svg class="absolute top-1 left-1 h-6 w-6 color-light-blue" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                       </svg>
-                      <a href="https://www.tereiken.be/" class="color-light-blue">{{__('home.clubContentPrefix3')}}:</a>
+                      <a href="https://www.tereiken.be/"  target="blank" class="color-light-blue">{{__('home.clubContentPrefix3')}}:</a>
                     </dt>
                     <dd class="inline">{{__('home.clubContent3')}}</dd>
                   </div>
@@ -206,7 +206,7 @@
                       <svg class="absolute top-1 left-1 h-6 w-6 color-light-blue" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                       </svg>
-                      <a href="https://garrincha.be/" class="color-light-blue">{{__('home.clubContentPrefix5')}}:</a>
+                      <a href="https://garrincha.be/"  target="blank" class="color-light-blue">{{__('home.clubContentPrefix5')}}:</a>
                     </dt>
                     <dd class="inline">{{__('home.clubContent5')}}</dd>
                   </div>
@@ -228,7 +228,7 @@
                       <svg class="absolute top-1 left-1 h-6 w-6 color-light-blue" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                       </svg>
-                      <a href="https://www.royalzoutetennisclub.be/" class="color-light-blue">  {{__('home.clubContentPrefix7')}}:</a>
+                      <a href="https://www.royalzoutetennisclub.be/"  target="blank" class="color-light-blue">  {{__('home.clubContentPrefix7')}}:</a>
                     </dt>
                     <dd class="inline">{{__('home.clubContent7')}}</dd>
                   </div>
@@ -257,7 +257,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                     </svg>
                   </div>
-                  <a href="https://planetpadel.be/" class="color-light-blue"> {{__('home.clubContentPrefix1')}} ( Planet Padel) </a>
+                  <a href="https://planetpadel.be/"  target="blank" class="color-light-blue"> {{__('home.clubContentPrefix1')}} ( Planet Padel) </a>
                 </dt>
                 <dd class="mt-0 text-base leading-7 text-gray-600">
                   <b>{{__('home.when')}}: </b> {{__('home.agendaWhen1')}}<br/>
@@ -275,7 +275,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                     </svg>
                   </div>
-                  <a href="https://www.tourtaxispadel.brussels/" class="color-light-blue"> {{__('home.clubContentPrefix2')}} (Tour & Taxis Padel Club) </a>
+                  <a href="https://www.tourtaxispadel.brussels/"  target="blank" class="color-light-blue"> {{__('home.clubContentPrefix2')}} (Tour & Taxis Padel Club) </a>
                 </dt>
                 
                 <dd class="mt-0 text-base leading-7 text-gray-600">
@@ -292,7 +292,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                     </svg>
                   </div>
-                  <a href="https://www.tereiken.be/" class="color-light-blue"> {{__('home.clubContentPrefix3')}} (Ter Eiken Padel)</a>
+                  <a href="https://www.tereiken.be/"  target="blank" class="color-light-blue"> {{__('home.clubContentPrefix3')}} (Ter Eiken Padel)</a>
                 </dt>
                 
                 <dd class="mt-0 text-base leading-7 text-gray-600">
@@ -308,7 +308,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                     </svg>
                   </div>
-                  <a href="https://teropadelclub.be/fr" class="color-light-blue"> {{__('home.clubContentPrefix4')}} (Tero)</a>
+                  <a href="https://teropadelclub.be/fr"  target="blank" class="color-light-blue"> {{__('home.clubContentPrefix4')}} (Tero)</a>
                 </dt>
                 
                 <dd class="mt-0 text-base leading-7 text-gray-600">
@@ -324,7 +324,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                     </svg>
                   </div>
-                  <a href="https://garrincha.be/" class="color-light-blue"> {{__('home.clubContentPrefix5')}} (Garrincha)</a>
+                  <a href="https://garrincha.be/"  target="blank" class="color-light-blue"> {{__('home.clubContentPrefix5')}} (Garrincha)</a>
                 </dt>
                 
                 <dd class="mt-0 text-base leading-7 text-gray-600">
@@ -340,7 +340,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
                     </svg>
                   </div>
-                  <a href="https://elevenpadel.be/" class="color-light-blue">{{__('home.clubContentPrefix6')}} (Eleven Padel)</a>
+                  <a href="https://elevenpadel.be/"  target="blank" class="color-light-blue">{{__('home.clubContentPrefix6')}} (Eleven Padel)</a>
                 </dt>
                 
                 <dd class="mt-0 text-base leading-7 text-gray-600">
@@ -358,7 +358,7 @@
                       <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0"></path>
                     </svg>
                   </div>
-                  <a href="https://www.royalzoutetennisclub.be/" class="color-light-blue"> {{__('home.thefinale')}} (Royal Zoute Tennis & Padel Club)</a>
+                  <a href="https://www.royalzoutetennisclub.be/" target="blank" class="color-light-blue"> {{__('home.thefinale')}} (Royal Zoute Tennis & Padel Club)</a>
                 </dt>
                 
                 <dd class="mt-0 text-base leading-7 text-gray-600">
