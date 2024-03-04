063c26515973f53245e3762fc212c8dfa169e228
Thomas Claessens <tom@itcl.io>
2024-03-04 09:44:36 +0100

    ...

--- docker/prod/run.sh
+++ docker/prod/run.sh
@@ -9,4 +9,6 @@ docker rm -f ${name}
 docker build --file=${APP_ROOT}"/docker/prod/php8.2/Dockerfile"  -t padel/php .
 docker run --name ${name} \
   -v ${APP_ROOT}:/var/www/html \
+  -v /data/nfs/businesspadeltour/images:/var/www/html/public/images \
+  -v /data/nfs/businesspadeltour/videos:/var/www/html/public/videos \
   -it padel/php 
--- resources/views/livewire/video.blade.php
+++ resources/views/livewire/video.blade.php
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 text-center">
         <h1 class="text-2xl text-slate-800 py-7 font-bold">Businesspadeltour Knokke Heist 2023</h1>
         <video width="100%" height="240" controls>
-            <source src="{{env('APP_URL').'/2023.mp4'}}" type=video/mp4>
+            <source src="{{env('APP_URL').'/videos/2023.mp4'}}" type=video/mp4>
         </video>
     </div>
 </div>
