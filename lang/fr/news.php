2a04239598c2fb2704a039727fc010486e4e6a39
Thomas Claessens <tom@itcl.io>
2024-08-19 15:45:55 +0200

    ...

--- lang/fr/news.php
+++ lang/fr/news.php
@@ -2,5 +2,5 @@
 
 return [
     'title' => 'L\'actualité du padel tour',
-    'teaser' => 'Les denières  infos et news sur le sujet',
+    'teaser' => 'Les dernières  infos et news sur le sujet',
 ];
