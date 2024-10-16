3dbeb76b9a3930c9a1f9bad189e0ca4613cc948a
Thomas Claessens <tom@itcl.io>
2024-10-16 12:14:34 +0200

    ...

--- docker/prod/run.sh
+++ docker/prod/run.sh
@@ -11,4 +11,8 @@ docker run --name ${name} \
   -v ${APP_ROOT}:/var/www/html \
   -v /data/nfs/businesspadeltour/images:/var/www/html/public/images \
   -v /data/nfs/businesspadeltour/videos:/var/www/html/public/videos \
+  -v /data/nfs/businesspadeltour/storage/app:/var/www/html/storage/app \
+  -v /data/volumes/businesspadeltour/livewire:/var/www/html/storage/app/livewire \
+  -v /data/volumes/businesspadeltour/livewire-tmp:/var/www/html/storage/app/livewire-tmp \
+
   -it padel/php 
