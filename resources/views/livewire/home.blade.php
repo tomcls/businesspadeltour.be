91b5017fafd68266e22495911eb37b1b00a02e06
Thomas Claessens <tom@itcl.io>
2024-10-23 22:04:02 +0200

    ...

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -465,21 +465,35 @@
       <div class="bg-white " id="sponsor">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="fadinAnimation opacity-0 text-pretty text-5xl font-medium tracking-tight  sm:text-6xl color-dark-blue pb-7">{{__('home.sponsorTitle')}}</h2>
+            <h2 class="fadinAnimation opacity-0 text-pretty text-5xl font-medium tracking-tight  sm:text-6xl color-dark-blue pb-7">{{__('An organization of')}}</h2>
           </div>
           <div class="flex flex-1 mt-10">
-            <ul role="list" class="sm:grid sm:grid-cols-6  sm:gap-4 sm:items-center justify-center  sm:border-t sm:border-gray-200 sm:py-5   sm:space-x-12 fadinAnimation opacity-0">
+            <ul role="list" class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-center justify-center  sm:border-t sm:border-gray-200 sm:py-5   sm:space-x-12 fadinAnimation opacity-0">
+              <li>
+                <a href="https://vertuosa.be/"  target="_blank" ><img class="mx-auto  items-center justify-center mt-7 h-20" src="{{url('/').'/images/sponsors/Vertuoza-Logo.png'}}" alt=""></a>
+              </li>
+              <li>
+                <a href="https://brugge.arenal.be/" target="_blank"  ><img class="mx-auto items-center justify-center mt-7 h-28" src="https://brugge.arenal.be/themes/custom/arenal/logo-new.svg" alt=""></a>
+              </li>
+              <li>
+                <a href="https://immo.vlan.be/fr" target="_blank"  ><img class="mx-auto items-center justify-center mt-7 h-14" src="{{url('/').'/images/sponsors/immovlan_mauve.png'}}"  alt=""></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="bg-white pt-20" >
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="fadinAnimation opacity-0 text-pretty text-5xl font-medium tracking-tight  sm:text-6xl color-dark-blue pb-7">{{__('In partnership with')}}</h2>
+          </div>
+          <div class="flex flex-1 mt-10">
+            <ul role="list" class="sm:grid sm:grid-cols-4  sm:gap-4 sm:items-center justify-center  sm:border-t sm:border-gray-200 sm:py-5   sm:space-x-12 fadinAnimation opacity-0">
+              <li>
+                <a href="https://wilmeyer.com/" target="_blank"  ><img class="mx-auto items-center justify-center mt-7" src="{{url('/').'/images/sponsors/wilmeyer.png'}}" alt=""></a>
+              </li>
               <li>
                 <a href="https://immo.vlan.be/fr" target="_blank"  ><img class="mx-auto items-center justify-center mt-7" src="{{url('/').'/images/sponsors/immovlan_mauve.png'}}"  alt=""></a>
-              </li>
-              <li>
-                <a href="https://wilmeyer.com/"  target="_blank" ><img class="mx-auto  items-center justify-center mt-7" src="{{url('/').'/images/sponsors/Vertuoza-Logo.png'}}" alt=""></a>
-              </li>
-              <li>
-                <a href="https://brugge.arenal.be/" target="_blank"  ><img class="mx-auto items-center justify-center mt-7" src="{{url('/').'/images/sponsors/wilmeyer.png'}}" alt=""></a>
-              </li>
-              <li>
-                <a href="https://brugge.arenal.be" target="_blank" ><img class="mx-auto items-center justify-center mt-7" src="https://brugge.arenal.be/themes/custom/arenal/logo-new.svg" alt=""></a>
               </li>
               <li>
                 <a href="https://santospalaceshop.be/bruxelles/" target="_blank"  ><img class="mx-auto  max-h-20 items-center justify-center mt-7" src="{{url('/').'/images/sponsors/santos.png'}}" alt=""></a>
