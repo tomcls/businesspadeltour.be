ccace73528efbdad60d40487b65d9c907b0bc403
Thomas Claessens <tom@itcl.io>
2024-10-23 13:46:59 +0200

    ...

---
+++ resources/views/components/icon/bars2.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes->merge(['class' => 'inline-block']) }} data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"></path>
+</svg>
