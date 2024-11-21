7f42272c48ef7a83858d0bc62dbb658bdbcf9eec
Don Classius <tom@itcl.io>
2024-11-21 10:59:02 +0100

    ...

--- app/Jobs/WelcomeEvent2025Job.php
+++ app/Jobs/WelcomeEvent2025Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\EventUser;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -33,47 +34,54 @@ class WelcomeEvent2025Job implements ShouldQueue
     {
         $mailchimp = new \MailchimpTransactional\ApiClient();
         $mailchimp->setApiKey(env('MAILCHIMP_APIKEY'));
-        $cpt =0;
-       // $users = User::get();
+        $cpt = 0;
+        // $users = User::get();
         $users = User::whereLang('fr')->get();
 
-        logger($users);
+       // logger($users);
         foreach ($users as $key => $user) {
-            // player one
-            $template_content = array(
-                array(
-                    'name' => 'url',
-                    'content' => 'https://businesspadeltour.be/'.$user->lang.'/welcome-event?id='.$user->id
-                )
-            );
-            $to = [];
-            $email = trim(strtolower( $user->email));
-            array_push($to, [
-                "email" =>  $email,
-                "type" => "to"
-            ]);
-           
-            $message = [
-                "from_email" => "info@businesspadeltour.be",
-                'from_name'  => 'Vertuoza padel tour',
-                "subject" => 'Vertuoza Padel Tour 2025, demandez le programme!',
-                "to" => $to,
-                "headers" => ["Reply-To" => "info@businesspadeltour.be"],
-                'global_merge_vars' => $template_content
-            ];
-            $cpt ++;
-            try {
-                logger('mail sent to '.$email. " total = ".$cpt);
-                // Log::alert('Player 1 email sent '.App::currentLocale());
-                $response = $mailchimp->messages->sendTemplate([
-                    "template_name" => "vertuoza_padel_event_2025_".$user->lang,
-                    "template_content" => $template_content,
-                    "message" => $message,
+
+            $eventuser = EventUser::whereUserId($user->id)->first();
+            logger($eventuser);
+            if ($eventuser && ($eventuser->event_id == 2 || $eventuser->event_id == 4) ) {
+
+                // player one
+                $template_content = array(
+                    array(
+                        'name' => 'url',
+                        'content' => 'https://businesspadeltour.be/' . $user->lang . '/welcome-event?id=' . $user->id
+                    )
+                );
+                $to = [];
+                $email = trim(strtolower($user->email));
+                array_push($to, [
+                    "email" =>  $email,
+                    "type" => "to"
                 ]);
-                logger($response);
-            } catch (\Throwable $th) {
-                //throw $th;
-                logger('email not sent to '.$email);
+
+                $message = [
+                    "from_email" => "info@businesspadeltour.be",
+                    'from_name'  => 'Vertuoza padel tour',
+                    "subject" => 'Kick off Vertuoza Padel Tour 2025',
+                    "to" => $to,
+                    "headers" => ["Reply-To" => "info@businesspadeltour.be"],
+                    'global_merge_vars' => $template_content
+                ];
+                $cpt++;
+                try {
+                    logger('mail sent to ' . $email . " total = " . $cpt);
+                    // Log::alert('Player 1 email sent '.App::currentLocale());
+                    $mailchimp->messages->sendTemplate([
+                        "template_name" => "vertuoza_padel_event_beready_" . $user->lang,
+                        "template_content" => $template_content,
+                        "message" => $message,
+                    ]);
+                    // logger($response);
+                    logger($cpt);
+                } catch (\Throwable $th) {
+                    //throw $th;
+                    logger('email not sent to ' . $email);
+                }
             }
         }
     }
